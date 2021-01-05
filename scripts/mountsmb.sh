c982d01d2cb0e0611dfe09258b7b7806788291c3
BerryFlake <36912056+berryflake@users.noreply.github.com>
2021-01-05 21:35:37 +0800

    Update mountsmb.sh

--- scripts/mountsmb.sh
+++ scripts/mountsmb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Mount smb network drive
 # This script is the automated version of my guide.
-# The guide link https://github.com/berryflake/linuxgoody/guides/mount_network_drive.md 
+# The guide https://github.com/berryflake/linuxgoody/blob/main/guides/mount_network_drive.md
 # This script is only tested with popos 20.04 in a VM. Thus, if you find an issue, report it to me.
 # Run this script as root, or sudo?(not sure will need testing), for now, use root user.
 # This script is only support ubuntu based system e.g. ubuntu, popos...
