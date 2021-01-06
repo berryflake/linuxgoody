d462b19f8dafc2a45cf7219913c4c39644a81749
BerryFlake <36912056+berryflake@users.noreply.github.com>
2021-01-06 15:22:45 +0800

    Update mountsmb.sh

--- scripts/mountsmb.sh
+++ scripts/mountsmb.sh
@@ -2,9 +2,8 @@
 # Mount smb network drive
 # This script is the automated version of my guide.
 # The guide https://github.com/berryflake/linuxgoody/blob/main/guides/mount_network_drive.md
-# This script is only tested with popos 20.04 in a VM. Thus, if you find an issue, report it to me.
 # Run this script as root, or sudo?(not sure will need testing), for now, use root user.
-# This script is only support ubuntu based system e.g. ubuntu, popos...
+# This script is only tested ubuntu based system e.g. ubuntu, popos...
 # Version 0.1 alpha
 
 # Set default variables
@@ -107,13 +106,12 @@ do
     case $YN_USRINFO in
         [yY]|[yY][eE][sS])
         # if yes, break the loop.
-        echo "info is accepted."
+        echo "info saved."
         break 
         ;;
         [nN]|[nN][oO])
-        # if no, ask user to input naming and run the while again.
+        # if no, ask user to input naming again.
         echo "Changing info"
-        # find your username and user id
         ;;
         *)
         # in case of unexpected input.
